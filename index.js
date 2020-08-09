dbfe3516c7efcb07de7edf1f3c811445e54e1151
ibrahim aljarad <ibrahimealkalil@gmail.com>
2020-08-09 17:53:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+  
+// ]
+// function superbowlWin(arr){
+//    if (arr.find(w => w === "W"){
+//      return `${arr.year} a win!`
+//    }else{
+//     return "undifine"
+//    }
+     
+// }
+
+function superbowlWin(arr) {
+	const foundObj = arr.find(function(ele) {
+		return ele['result'] === 'W';
+	});
+	if (foundObj) {
+		return foundObj['year'];
+	}
 }
